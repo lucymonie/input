a7818b2189040a167b720fbc35e88a4129d1655f
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2017-10-04 18:19:57 +0100

    Initial commit of create-react-app files

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+class SubmitName extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      name: 'there',
+      textEntered: ''
+    }
+  }
+
+  onChange (event) {
+    event.preventDefault();
+    this.setState({
+      textEntered: event.target.value
+    })
+  }
+
+  onSubmit (event) {
+    event.preventDefault();
+    this.setState({
+      name: this.state.textEntered,
+      textEntered: ''
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <Greeting name={this.state.name}/>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input value={this.state.textEntered} onChange={this.onChange.bind(this)} />
+          <button>Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+class Greeting extends React.Component {
+  render () {
+    return (
+      <div>
+        <h2>Greeting: Hi {this.props.name}</h2>
+      </div>
+    )
+  }
+}
+
+
+const app = document.getElementById('root');
+ReactDOM.render(<SubmitName />, app)
